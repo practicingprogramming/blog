f7d209406ea6a52b74690fd2f88417bbc044af19
practicingprogramming <practicingprogramming@gmail.com>
2015-12-27 16:06:06 -0800

    Serve posts

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -3,6 +3,8 @@ var blogControllers = angular.module('blogControllers');
 blogControllers.controller('PostController', ['$scope', '$routeParams', 'postService',
     function ($scope, $routeParams, postService) {
         var postId = $routeParams.postId;
+
         $scope.post = postService.getPost(postId); // TODO: handle post not found
+        $scope.partial = '/partials/posts/' + postId + '.html';
     }
 ]);
--- js/services/postService.js
+++ js/services/postService.js
@@ -1,11 +1,17 @@
 angular
 .module('blog')
 .factory('postService', function() {
+    var posts = {
+        'testPost1': {
+            title: 'This is the first post'
+        },
+        'testPost2': {
+            title: 'This is the second post'
+        }
+    };
     return {
         getPost: function(id) {
-            return {
-                title: id + '\' title'
-            };
+            return posts[id];
         }
     };
 });
