214639937ba5e72a91a27bb2dce4171d09062e54
practicingprogramming <practicingprogramming@gmail.com>
2015-12-28 22:55:04 -0800

    Hyperlinks to posts from the front page

--- js/services/postService.js
+++ js/services/postService.js
@@ -1,21 +1,26 @@
 angular
 .module('blog')
 .factory('postService', function() {
+    // TODO: ids are duplicated
+    // TODO: partials can be inferred automatically
     var posts = {
         'testPost1': {
             title: 'This is the first post',
             timestamp: 11111,
-            partial: '/partials/posts/testPost1.html'
+            partial: '/partials/posts/testPost1.html',
+            id: 'testPost1'
         },
         'testPost2': {
             title: 'This is the second post',
             timestamp: 33333,
-            partial: '/partials/posts/testPost2.html'
+            partial: '/partials/posts/testPost2.html',
+            id: 'testPost2'
         },
         'creatingThisBlog': {
             title: 'Creating This Blog',
             timestamp: 22222,
-            partial: '/partials/posts/creatingThisBlog.html'
+            partial: '/partials/posts/creatingThisBlog.html',
+            id: 'creatingThisBlog'
         }
     };
     return {
