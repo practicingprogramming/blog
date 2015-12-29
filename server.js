a74c374046f91efba1aa02051539958474a9cf61
practicingprogramming <practicingprogramming@gmail.com>
2015-12-28 21:49:42 -0800

    Some styling

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var app = express();
 var path = require('path');
 app.use(express.static('public'));
 app.use(express.static('js'));
+app.use(express.static('css'));
 app.listen(8080);
 console.log('App listening on port 8080');
