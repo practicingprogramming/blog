78968e42ae9f7f105b97071c1134a1bbaa8c50bc
practicingprogramming <practicingprogramming@gmail.com>
2015-12-28 22:15:27 -0800

    Publish date

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -6,5 +6,13 @@ blogControllers.controller('PostController', ['$scope', '$routeParams', 'postSer
 
         $scope.post = postService.getPost(postId); // TODO: handle post not found
         $scope.partial = '/partials/posts/' + postId + '.html';
+
+        $scope.timestampToDate = function(timestamp) {
+            var a = new Date(timestamp * 1000);
+            var year = a.getFullYear();
+            var month = a.getMonth() + 1;
+            var date = a.getDate();
+            return year + '/' + month + '/' + date;
+        };
     }
 ]);
--- js/services/postService.js
+++ js/services/postService.js
@@ -3,13 +3,16 @@ angular
 .factory('postService', function() {
     var posts = {
         'testPost1': {
-            title: 'This is the first post'
+            title: 'This is the first post',
+            timestamp: 1451369142
         },
         'testPost2': {
-            title: 'This is the second post'
+            title: 'This is the second post',
+            timestamp: 1251360142
         },
         'creatingThisBlog': {
-            title: 'Creating This Blog'
+            title: 'Creating This Blog',
+            timestamp: 1451269142
         }
     };
     return {
