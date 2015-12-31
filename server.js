008073585d2eb4a6de666d01f521033ce4702066
practicingprogramming <practicingprogramming@gmail.com>
2015-12-30 20:57:55 -0800

    Point server.js to build

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static('public'));
-app.use(express.static('js'));
-app.use(express.static('css'));
+app.use(express.static('build'));
 app.listen(8080);
 console.log('App listening on port 8080');
