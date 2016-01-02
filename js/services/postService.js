e2d76da9e1925faba0e6c51fba08d32e2f1cf254
practicingprogramming <practicingprogramming@gmail.com>
2016-01-02 11:33:45 -0800

    Remove test posts

--- js/services/postService.js
+++ js/services/postService.js
@@ -4,18 +4,6 @@ angular
     // TODO: ids are duplicated
     // TODO: partials can be inferred automatically
     var posts = {
-        'testPost1': {
-            title: 'This is the first post',
-            timestamp: 11111,
-            partial: '/partials/posts/testPost1.html',
-            id: 'testPost1'
-        },
-        'testPost2': {
-            title: 'This is the second post',
-            timestamp: 33333,
-            partial: '/partials/posts/testPost2.html',
-            id: 'testPost2'
-        },
         'creatingThisBlog': {
             title: 'Creating This Blog',
             timestamp: 22222,
